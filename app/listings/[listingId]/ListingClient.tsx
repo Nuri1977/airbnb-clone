5f4a5c2ce65c52b9584766a4689a2bb373041cc0
nurilacka-admin <nuri@adaptivescale.com>
2023-07-22 15:34:51 +0200

    reservation screen

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -76,6 +76,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
       .then(() => {
         toast.success("Listing reserved!");
         setDateRange(initialDateRange);
+        router.refresh();
         router.push("/trips");
       })
       .catch((err) => {
